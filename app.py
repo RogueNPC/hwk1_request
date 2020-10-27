1d279d744dfbf2b441ddc62d1b8e1881b569da8f
RogueNPC <jordanbtansay@gmail.com>
2020-10-26 17:00:05 -0700

    added tutorial code

--- app.py
+++ app.py
@@ -3,5 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    """Shows a greeting to the user."""
     return 'Are you there, world? It\'s me, Ducky!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
