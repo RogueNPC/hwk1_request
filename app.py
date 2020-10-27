116920bfd8e59d0c32bf4f94bd808f5cf5232fde
RogueNPC <jordanbtansay@gmail.com>
2020-10-26 17:13:33 -0700

    completed favorite animal, favorite dessert, and madlibs exercise

--- app.py
+++ app.py
@@ -5,5 +5,21 @@ app = Flask(__name__)
 def homepage():
     return 'Are you there, world? It\'s me, Ducky!'
 
+@app.route('/polar_bears')
+def favoriteAnimal():
+    return 'Polar Bears are cute!'
+
+@app.route('/animal/<users_animal>')
+def favorite_animal(users_animal):
+    return f'Wow, {users_animal} are my favorite animal, too!'
+
+@app.route('/dessert/<users_dessert>')
+def favorite_desert(users_dessert):
+    return f'How did you know I liked {users_dessert}?'
+
+@app.route('/madlibs/<adjective>/<noun>')
+def madlibs(adjective, noun):
+    return f'The sitcom about the {noun} never took off, because the pilot was {adjective}.'
+
 if __name__ == '__main__':
     app.run(debug=True)
